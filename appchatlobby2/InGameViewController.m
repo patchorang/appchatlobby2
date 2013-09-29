04126da53710d24467d803654aef8c9448a4115f
Adam Menz <mz.adam@gmail.com>
2013-09-28 20:55:52 -0400

    calculating motion

--- appchatlobby2/InGameViewController.m
+++ appchatlobby2/InGameViewController.m
@@ -14,6 +14,8 @@
 
 @implementation InGameViewController
 
+@synthesize scoreLabel;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,12 +29,46 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    [self becomeFirstResponder];
+    
+    currentMaxAccelX = 0;
+    currentMaxAccelY = 0;
+    currentMaxAccelZ = 0;
+    score = 0;
+    
+    self.motionManager = [[CMMotionManager alloc] init];
+    self.motionManager.accelerometerUpdateInterval = .2;
+    
+    [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMGyroData *gyroData, NSError *error) {
+        [self outputRotationData:gyroData.rotationRate];
+    }];
+    
 }
 
-- (void)didReceiveMemoryWarning
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if (event.subtype == UIEventTypeMotion){
+        NSLog(@"MOtion");
+    }
+}
+
+-(void)outputRotationData:(CMRotationRate)rotation
+{
+    currentMaxRotX = rotation.x;
+    currentMaxRotY = rotation.y;
+    currentMaxRotZ = rotation.z;
+    
+    double gyroVector = sqrt(pow(rotation.x, 2) + pow(rotation.x, 2) + pow(rotation.x, 2));
+    if (gyroVector > .5){
+        score += gyroVector;
+    }
+    
+    scoreLabel.text = [NSString stringWithFormat:@"%d", ((int)score)/10];
 }
 
 @end
